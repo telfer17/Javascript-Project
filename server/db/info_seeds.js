d3a33217617dc8ed4751f99a678e762b6a69369a
David Telfer <davietelfer@hotmail.com>
2018-10-26 14:17:13 +0100

    updated question structure in seeds file

--- server/db/info_seeds.js
+++ server/db/info_seeds.js
@@ -1,77 +1,77 @@
 use solarsystem
 
-db.dropdatabase();
+db.dropDatabase();
 
 db.planet_info.insertMany( [
   {
     name: "Mercury",
-    Solar System position from Sun: 1,
-    Orbital period round sun: "87.97 days",
-    Surface Area: "7.48×107 km2",
-    Volume: "6.083×1010 km3",
-    Mass: "3.3011×1023 kg"
+    position: 1,
+    orbit: "87.97 days",
+    surfaceArea: "7.48×107 km2",
+    volume: "6.083×1010 km3",
+    mass: "3.3011×1023 kg"
   },
 
   {
     name: "Venus",
-    Solar System position from Sun: 2,
-    Orbital period round sun: "224.7 earth days",
-    Surface Area: "4.6023×108 km2",
-    Volume: "9.2843×1011 km3",
-    Mass: "4.8675×1024 kg"
+    position: 2,
+    orbit: "224.7 earth days",
+    surfaceArea: "4.6023×108 km2",
+    volume: "9.2843×1011 km3",
+    mass: "4.8675×1024 kg"
   },
 
   {
     name: "Earth",
-    Solar System position from Sun: 3,
-    Orbital period round sun: "365.26 Earth days",
-    Surface Area: "510072000 km2",
-    Volume: "	1.08321×1012 km3",
-    Mass: "5.97237×1024 kg"
+    position: 3,
+    orbit: "365.26 Earth days",
+    surfaceArea: "510072000 km2",
+    volume: "	1.08321×1012 km3",
+    mass: "5.97237×1024 kg"
   },
 
   {
     name: "Mars",
-    Solar System position from Sun: 4,
-    Orbital period round sun: "686.97 Earth days",
-    Surface Area: "144798500 km2",
-    Volume: "1.6318×1011 km3",
-    Mass: "6.4171×1023 kg"
+    position: 4,
+    orbit: "686.97 Earth days",
+    surfaceArea: "144798500 km2",
+    volume: "1.6318×1011 km3",
+    mass: "6.4171×1023 kg"
   },
 
   {
     name: "Jupiter",
-    Solar System position from Sun: 5,
-    Orbital period round sun: "4,332.59 Earth days",
-    Surface Area: "6.1419×1010 km2",
-    Volume: "1.4313×1015 km3",
-    Mass: "1.8982×1027 kg"
+    position: 5,
+    orbit: "4,332.59 Earth days",
+    surfaceArea: "6.1419×1010 km2",
+    volume: "1.4313×1015 km3",
+    mass: "1.8982×1027 kg"
   },
 
   {
     name: "Saturn",
-    Solar System position from Sun: 6,
-    Orbital period round sun: "10,759.22 Earth days",
-    Surface Area: "4.27×1010 km2",
-    Volume: "8.2713×1014 km3",
-    Mass: "5.6834×1026 kg "
+    position: 6,
+    orbit: "10,759.22 Earth days",
+    surfaceArea: "4.27×1010 km2",
+    volume: "8.2713×1014 km3",
+    mass: "5.6834×1026 kg "
   },
 
   {
     name: "Uranus",
-    Solar System position from Sun: 7,
-    Orbital period round sun: "30,688.5 Earth days",
-    Surface Area: "8.1156×109 km2",
-    Volume: "6.833×1013 km3",
-    Mass: "(8.6810±0.0013)×1025 kg"
+    position: 7,
+    orbit: "30,688.5 Earth days",
+    surfaceArea: "8.1156×109 km2",
+    volume: "6.833×1013 km3",
+    mass: "(8.6810±0.0013)×1025 kg"
   },
 
   {
     name: "Neptune",
-    Solar System position from Sun: 8,
-    Orbital period round sun: "60,182 Earth days",
-    Surface Area: "7.6183×109 km2",
-    Volume: "6.254×1013 km3",
-    Mass: "1.02413×1026 kg"
+    position: 8,
+    orbit: "60,182 Earth days",
+    surfaceArea: "7.6183×109 km2",
+    volume: "6.254×1013 km3",
+    mass: "1.02413×1026 kg"
   }
-])
+]);
--- server/db/question_seeds.js
+++ server/db/question_seeds.js
@@ -1,87 +1,93 @@
 use solarsystem;
-db.dropDatabase();
+// db.dropDatabase();
 
 db.planet_questions.insertMany([
-{
-question: "Which planet is closest to the Sun?",
-correct_answer: "Mercury",
-incorrect_answers: [
-"Venus",
-"Mars",
-"Saturn"
-]
-},
+  {
+    question: "Which planet is closest to the Sun?",
+    correct_answer: "Mercury",
+    answers: [
+      "Venus",
+      "Mercury",
+      "Mars",
+      "Saturn"
+    ]
+  },
 
-{
-question: "Which planet is farthest from the Sun?",
-correct_answer: "Neptune",
-incorrect_answers: [
-"Mars",
-"Uranus",
-"Saturn"
-]
-},
+  {
+    question: "Which planet is farthest from the Sun?",
+    correct_answer: "Neptune",
+    answers: [
+      "Mars",
+      "Uranus",
+      "Neptune",
+      "Saturn"
+    ]
+  },
 
-{
-question: "Which planet has the smallest surface area?",
-correct_answer: "Mercury",
-incorrect_answers: [
-"Mars",
-"Neptune",
-"Earth"
-]
-},
+  {
+    question: "Which planet has the smallest surface area?",
+    correct_answer: "Mercury",
+    answers: [
+      "Mars",
+      "Neptune",
+      "Earth",
+      "Mercury"
+    ]
+  },
 
-{
-question: "Which planet has the largest surface area?",
-correct_answer: "Jupiter",
-incorrect_answers: [
-"Saturn",
-"Mars",
-"Earth"
-]
-},
+  {
+    question: "Which planet has the largest surface area?",
+    correct_answer: "Jupiter",
+    answers: [
+      "Saturn",
+      "Jupiter",
+      "Mars",
+      "Earth"
+    ]
+  },
 
-{
-question: "Which planet takes almost twice as long as Earth to orbit the Sun?",
-correct_answer: "Mars",
-incorrect_answers: [
-"Venus",
-"Saturn",
-"Jupiter"
-]
-},
+  {
+    question: "Which planet takes almost twice as long as Earth to orbit the Sun?",
+    correct_answer: "Mars",
+    answers: [
+      "Venus",
+      "Saturn",
+      "Jupiter",
+      "Mars"
+    ]
+  },
 
-{
-question: "Which planet has longest orbital period around the Sun?",
-correct_answer: "Neptune",
-incorrect_answers: [
-"Uranus",
-"Jupiter",
-"Saturn"
-]
-},
+  {
+    question: "Which planet has longest orbital period around the Sun?",
+    correct_answer: "Neptune",
+    answers: [
+      "Uranus",
+      "Jupiter",
+      "Neptune",
+      "Saturn"
+    ]
+  },
 
-{
-question: "Which planet can sustain life?",
-correct_answer: "Earth",
-incorrect_answers: [
-"Mars",
-"Neptune",
-"Jupiter"
-]
-},
+  {
+    question: "Which planet can sustain life?",
+    correct_answer: "Earth",
+    answers: [
+      "Mars",
+      "Neptune",
+      "Earth",
+      "Jupiter"
+    ]
+  },
 
-{
-question: "Bonus! Which planet was subject to a movie starring Matt Damon?",
-correct_answer: "Mars",
-incorrect_answers: [
-"Saturn",
-"Venus",
-"Mercury"
-]
-}
+  {
+    question: "Bonus! Which planet was subject to a movie starring Matt Damon?",
+    correct_answer: "Mars",
+    answers: [
+      "Mars",
+      "Saturn",
+      "Venus",
+      "Mercury"
+    ]
+  }
 
-
-]
-)
+]);
