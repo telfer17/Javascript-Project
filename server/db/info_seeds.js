620db25fe96116d2a26cd16a02509c48b91aed24
Andrew Brown <alpha121bravo@gmail.com>
2018-10-26 12:59:47 +0100

    plannets data added

---
+++ server/db/info_seeds.js
@@ -0,0 +1,73 @@
+const planets = [
+  {
+    name: "Mercury",
+    Solar System position from Sun: 1,
+    Orbital period round sun: "87.97 days",
+    Surface Area: "7.48×107 km2",
+    Volume: "6.083×1010 km3",
+    Mass: "3.3011×1023 kg"
+  },
+
+  {
+    name: "Venus",
+    Solar System position from Sun: 2,
+    Orbital period round sun: "224.7 earth days",
+    Surface Area: "4.6023×108 km2",
+    Volume: "9.2843×1011 km3",
+    Mass: "4.8675×1024 kg"
+  },
+
+  {
+    name: "Earth",
+    Solar System position from Sun: 3,
+    Orbital period round sun: "365.26 Earth days",
+    Surface Area: "510072000 km2",
+    Volume: "	1.08321×1012 km3",
+    Mass: "5.97237×1024 kg"
+  },
+
+  {
+    name: "Mars",
+    Solar System position from Sun: 4,
+    Orbital period round sun: "686.97 Earth days",
+    Surface Area: "144798500 km2",
+    Volume: "1.6318×1011 km3",
+    Mass: "6.4171×1023 kg"
+  },
+
+  {
+    name: "Jupiter",
+    Solar System position from Sun: 5,
+    Orbital period round sun: "4,332.59 Earth days",
+    Surface Area: "6.1419×1010 km2",
+    Volume: "1.4313×1015 km3",
+    Mass: "1.8982×1027 kg"
+  },
+
+  {
+    name: "Saturn",
+    Solar System position from Sun: 6,
+    Orbital period round sun: "10,759.22 Earth days",
+    Surface Area: "4.27×1010 km2",
+    Volume: "8.2713×1014 km3",
+    Mass: "5.6834×1026 kg "
+  },
+
+  {
+    name: "Uranus",
+    Solar System position from Sun: 7,
+    Orbital period round sun: "30,688.5 Earth days",
+    Surface Area: "8.1156×109 km2",
+    Volume: "6.833×1013 km3",
+    Mass: "(8.6810±0.0013)×1025 kg"
+  },
+
+  {
+    name: "Neptune",
+    Solar System position from Sun: 8,
+    Orbital period round sun: "60,182 Earth days",
+    Surface Area: "7.6183×109 km2",
+    Volume: "6.254×1013 km3",
+    Mass: "1.02413×1026 kg"
+  }
+]
