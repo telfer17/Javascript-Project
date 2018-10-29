8f66d7cfb109b3182146caad0f3bddad52bfecf5
Andrew Brown <alpha121bravo@gmail.com>
2018-10-29 12:01:30 +0000

    questions and answers displaying

--- client/src/views/question_view.js
+++ client/src/views/question_view.js
@@ -5,13 +5,18 @@ const QuestionView = function (container) {
   this.container = container;
 }
 
-QuestionView.prototype.render = function (questionObject) {
+QuestionView.prototype.renderQuestions = function (questionObject) {
 
   const questionElement = document.createElement('div');
   questionElement.id = 'question-item';
 
   const questionTitle = this.createQuestion(questionObject.question);
   questionElement.appendChild(questionTitle);
+
+  console.log(questionObject.answers);
+
+  const answerList = this.showAnswers(questionObject.answers);
+  questionElement.appendChild(answerList);
 
   this.container.appendChild(questionElement);
 
@@ -25,3 +30,28 @@ QuestionView.prototype.createQuestion = function (text) {
   return questionText;
 
 };
+
+QuestionView.prototype.showAnswers = function (answersArray) {
+  let ul = document.createElement('ul');
+  answersArray.forEach((answer)=>{
+    // create an html element
+    const listItem = document.createElement('li');
+    // set the textContent of it
+    listItem.textContent = answer;
+    // add a radio button
+    const choiceSelection = document.createElement('input');
+
+    choiceSelection.setAttribute('type', 'radio');
+    choiceSelection.setAttribute('name', 'option');
+
+    //append the element to the ul
+    ul.appendChild(listItem);
+    ul.appendChild(choiceSelection)
+    //outside the loop, return the ul
+  })
+  return ul;
+
+};
+
+
+module.exports = QuestionView;
