171a9af18aa9c8f22aa0666185b1a1c6342ae84b
David Telfer <davietelfer@hotmail.com>
2018-10-31 10:43:03 +0000

    more info working

--- server/db/info_seeds.js
+++ server/db/info_seeds.js
@@ -10,6 +10,7 @@ db.planet_info.insertMany( [
     surfaceArea: "7.48×107 km2",
     volume: "6.083×1010 km3",
     mass: "3.3011×1023 kg",
+    fact: "Mercury is the most cratered planet in the Solar System",
     image: '/images/mercury.png'
   },
 
@@ -20,6 +21,7 @@ db.planet_info.insertMany( [
     surfaceArea: "4.6023×108 km2",
     volume: "9.2843×1011 km3",
     mass: "4.8675×1024 kg",
+    fact: ,
     image: '/images/venus.png'
   },
 
@@ -30,6 +32,7 @@ db.planet_info.insertMany( [
     surfaceArea: "510072000 km2",
     volume: "	1.08321×1012 km3",
     mass: "5.97237×1024 kg",
+    fact: "Earth is the only planet that wasn't named after a Greek or Roman god/goddess",
     image: '/images/earth.png'
   },
 
@@ -40,6 +43,7 @@ db.planet_info.insertMany( [
     surfaceArea: "144798500 km2",
     volume: "1.6318×1011 km3",
     mass: "6.4171×1023 kg",
+    fact: "Mars is red because it has a layer of rusty dust covering its entire surface",
     image: '/images/mars.png'
   },
 
@@ -50,6 +54,7 @@ db.planet_info.insertMany( [
     surfaceArea: "6.1419×1010 km2",
     volume: "1.4313×1015 km3",
     mass: "1.8982×1027 kg",
+    fact: ,
     image: '/images/jupiter.png'
   },
 
@@ -60,6 +65,7 @@ db.planet_info.insertMany( [
     surfaceArea: "4.27×1010 km2",
     volume: "8.2713×1014 km3",
     mass: "5.6834×1026 kg ",
+    fact: "Saturn's rings are made of billions of pieces of ice, dust and rocks",
     image: '/images/saturn.png'
   },
 
@@ -70,6 +76,7 @@ db.planet_info.insertMany( [
     surfaceArea: "8.1156×109 km2",
     volume: "6.833×1013 km3",
     mass: "(8.6810±0.0013)×1025 kg",
+    fact: ,
     image: '/images/uranus.png'
   },
 
@@ -80,6 +87,7 @@ db.planet_info.insertMany( [
     surfaceArea: "7.6183×109 km2",
     volume: "6.254×1013 km3",
     mass: "1.02413×1026 kg",
+    fact: ,
     image: '/images/neptune.png'
   }
 ]);
